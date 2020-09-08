27c17981f1115f3ada4466ecaf7d12d8bc5d4086
JeffreyChen-coding <70867119+JeffreyChen-coding@users.noreply.github.com>
2020-09-08 10:34:48 +0800

    Tkinter視窗按鈕點擊事件
    Tkinter視窗按鈕點擊事件

--- AE403-3.py
+++ AE403-3.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  8 09:04:09 2020
-
-"""
 
 import tkinter as tk
 import tkinter.messagebox
 
 def clickme():
     tkinter.messagebox.showinfo(title='訊息',message='按鈕已被點選')
-   
-    
+      
 window = tk.Tk()
 window.title('my first GUI')
 window.geometry('300x300')
-
-var = tk.StringVar()    # 將label標籤的內容設定為字元型別，用var來接收hit_me函式的傳出內容用以顯示在標籤上
 
 label = tk.Label(window, text='GUI')
 label.pack()
